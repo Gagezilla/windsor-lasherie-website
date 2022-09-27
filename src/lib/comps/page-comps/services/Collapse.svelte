a8d4c468a6478e3be9eeda910f7973d62de73863
Gage Hundertmark <hundertmarkgage@gmail.com>
2022-09-27 14:32:47 -0600

    fixed newline(br) issues

--- src/lib/comps/page-comps/services/Collapse.svelte
+++ src/lib/comps/page-comps/services/Collapse.svelte
@@ -4,24 +4,25 @@
 </script>
 
 <div class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-    <input type="checkbox" />
+    <input type="checkbox"/>
     <div class="collapse-title text-xl font-medium">
         {title}
     </div>
     <div class="collapse-content">
-        <div class="pt-5"></div>
-        {#if content}
-            {#each content as part, i}
-                {#if part.title && !part.content}
-                    <h3 class="font-bold px-10">{part.title}</h3>
-                    {#if i !== content.length-1}<br><br>{/if}
-                {:else if part.title && part.content}
-                    <h3 class="font-bold px-10">{part.title}</h3><br><br>
-                    <p class="inline-block px-10">{part.content}</p>
-                    {#if i !== content.length-1}<br><br>{/if}
-                {/if}
-            {/each}
-        {/if}
+        <div class="py-5">
+            {#if content}
+                {#each content as part, i}
+                    {#if part.title && !part.content}
+                        <h3 class="font-bold px-10">{part.title}</h3>
+                        {#if i !== content.length - 1}<br><br>{/if}
+                    {:else if part.title && part.content}
+                        <h3 class="font-bold px-10">{part.title}</h3><br>
+                        <p class="inline-block px-10">{part.content}</p>
+                        {#if i !== content.length - 1}<br><br><br>{/if}
+                    {/if}
+                {/each}
+            {/if}
+        </div>
         <slot/>
     </div>
 </div>
